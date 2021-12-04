f016d8e1cbf3122f9c730e2fcbb8491ddcce544c
Jared Koiter <jaredpkoiter@gmail.com>
2021-12-04 11:44:32 -0500

    2021-Day4 - Submit initial solution for puzzle 1

---
+++ 2021/Day 4/Run-Day4.ps1
@@ -0,0 +1,191 @@
+function Get-BoardData {
+    [CmdletBinding()]
+    param (
+        [string[]] $BoardInput,
+        [int] $BoardDimension
+    )
+
+    $boards = @()
+
+    $boardLines = @()
+    foreach ($i in (0..($BoardInput.Count-1))) {
+        $line = $BoardInput[$i]
+        if ($line) {
+            $boardLines += $line
+        }
+        if (-not $line -or ($i -eq ($BoardInput.Count-1))) {
+            if ($boardLines.Count -ne $BoardDimension) {
+                throw "incomplete board data, can't parse"
+            }
+
+            $board = @()
+            foreach ($boardLine in $boardLines) {
+                $board += ,(($boardLine -split ' ') | Where-Object { $_ })
+            }
+
+            $boards += ,$board
+            $boardLines = @()
+        }
+    }
+
+    return $boards
+}
+
+# for debugging purposes
+function Print-Board {
+    [CmdletBinding()]
+    param (
+        $Board
+    )
+
+    for ($rowNum = 0; $rowNum -lt $Board.Count; $rowNum++) {
+        for ($colNum = 0; $colNum -lt $Board.Count; $colNum++) {
+            Write-Host "$($Board[$rowNum][$colNum]) " -NoNewline
+        }
+        Write-Host
+    }
+}
+
+function Set-DrawMarks {
+    [CmdletBinding()]
+    param (
+        $NumberBoard,
+        $MarkedBoard,
+        $Draw
+    )
+
+    for ($rowNum = 0; $rowNum -lt $NumberBoard.Count; $rowNum++) {
+        # deep copy to avoid overwriting *all* rows in the board at the same time
+        $row = $MarkedBoard[$rowNum] | ForEach-Object { $_ }
+        for ($colNum = 0; $colNum -lt $row.Count; $colNum++) {
+            if ($NumberBoard[$rowNum][$colNum] -eq $Draw) {
+                $row[$colNum] = 1
+            }
+        }
+        $MarkedBoard[$rowNum] = $row
+    }
+
+    return $MarkedBoard
+}
+
+function Check-BoardForBingo {
+    [CmdletBinding()]
+    param (
+        $MarkedBoard
+    )
+
+    $winner = $false
+    $count = $MarkedBoard.Count
+
+    # check for row wins
+    for ($row = 0; $row -lt $count; $row++) {
+        if (($MarkedBoard[$row] | Measure-Object -Sum).Sum -eq $count) {
+            $winner = $true
+            break
+        }
+    }
+
+    if ($winner) {
+        return $winner
+    }
+
+    # check for column wins
+    for ($col = 0; $col -lt $count; $col++) {
+        $sum = 0
+        for ($row = 0; $row -lt $count; $row++) {
+            $sum += $MarkedBoard[$row][$col]
+        }
+
+        if ($sum -eq $count) {
+            $winner = $true
+            break
+        }
+    }
+
+    return $winner
+}
+
+function Get-BoardScore {
+    [CmdletBinding()]
+    param (
+        $NumberBoard,
+        $MarkedBoard,
+        $Draw
+    )
+
+    $sum = 0
+    for ($rowNum = 0; $rowNum -lt $MarkedBoard.Count; $rowNum++) {
+        for ($colNum = 0; $colNum -lt $MarkedBoard.Count; $colNum++) {
+            if (-not ($MarkedBoard[$rowNum][$colNum])) {
+                $sum += $NumberBoard[$rowNum][$colNum]
+            }
+        }
+    }
+
+    return ($sum * $Draw)
+}
+
+function Run-Puzzle1 {
+    [CmdletBinding()]
+    param (
+        $PuzzleInput
+    )
+
+    $boardDimension = 5
+    $draws = $PuzzleInput[0] -split ','
+
+    # parse the rest of the file to get the various board states
+    $boards = Get-BoardData -BoardInput ($PuzzleInput | Select-Object -Skip 2) -BoardDimension $boardDimension
+    $markedBoards = ,(,(,0 * $boardDimension) * $boardDimension) * $boards.Count
+
+    # draw the initial 5 without checking for bingos
+    for ($drawNum = 0; $drawNum -le 4; $drawNum++) {
+        $draw = $draws[$drawNum]
+
+        for ($boardNum = 0; $boardNum -lt $boards.Count; $boardNum++) {
+            $markedBoards[$boardNum] = Set-DrawMarks -NumberBoard $boards[$boardNum] -MarkedBoard ($markedBoards[$boardNum] | ForEach-Object { , $_ }) -Draw $draw
+        }
+    }
+
+    for ($boardNum = 0; $boardNum -lt $boards.Count; $boardNum++) {
+        if (Check-BoardForBingo -MarkedBoard $markedBoards[$boardNum]) {
+            return Get-BoardScore -NumberBoard $boards[$boardNum] -MarkedBoard $markedBoards[$boardNum] -Draw $draw
+        }
+    }
+
+    for ($drawNum = 5; $drawNum -lt $draws.Count; $drawNum++) {
+        $draw = $draws[$drawNum]
+        for ($boardNum = 0; $boardNum -lt $boards.Count; $boardNum++) {
+            $markedBoards[$boardNum] = Set-DrawMarks -NumberBoard $boards[$boardNum] -MarkedBoard ($markedBoards[$boardNum] | ForEach-Object { , $_ }) -Draw $draw
+            if (Check-BoardForBingo -MarkedBoard $markedBoards[$boardNum]) {
+                Print-Board $boards[$boardNum]
+                Print-Board $markedBoards[$boardNum]
+                Write-Host $draw
+                return Get-BoardScore -NumberBoard $boards[$boardNum] -MarkedBoard $markedBoards[$boardNum] -Draw $draw
+            }
+        }
+    }
+}
+
+function Run-Puzzle2 {
+    [CmdletBinding()]
+    param (
+        $PuzzleInput
+    )
+}
+
+[string[]]$puzzleInput = Get-Content .\input.txt
+
+$puzzle1Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle1 -PuzzleInput $puzzleInput
+$puzzle1Timer.Stop()
+
+Write-Host "Puzzle 1 Answer: $output"
+Write-Host "Time: $($puzzle1Timer.ElapsedMilliseconds) ms"
+
+$puzzle2Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle2 -PuzzleInput $puzzleInput
+$puzzle2Timer.Stop()
+
+Write-Host "Puzzle 2 Answer: $output"
+Write-Host "Time: $($puzzle2Timer.ElapsedMilliseconds) ms"
