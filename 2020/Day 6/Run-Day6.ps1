6ef68960d0eb9bb075f2b27f26ac6b2059295508
Jared Koiter <jaredpkoiter@gmail.com>
2021-12-05 16:39:42 -0500

    2020-Day6 - Add intial solution for day 6 puzzle 1

---
+++ 2020/Day 6/Run-Day6.ps1
@@ -0,0 +1,46 @@
+function Run-Puzzle1 {
+    [CmdletBinding()]
+    param (
+        $PuzzleInput
+    )
+    
+    $sum = 0
+    $groupInput = @()
+    for ($i = 0; $i -le $PuzzleInput.Count; $i++) {
+        $line = $PuzzleInput[$i]
+        if ($line) {
+            $groupInput += $line.ToCharArray()
+        }
+        else {
+            $sum += ($groupInput | Sort-Object | Get-Unique).Count
+            $groupInput = @()
+        }
+    }
+
+    return $sum
+}
+
+function Run-Puzzle2 {
+    [CmdletBinding()]
+    param (
+        $PuzzleInput
+    )
+
+    #TODO
+}
+
+[string[]]$puzzleInput = Get-Content .\input.txt
+
+$puzzle1Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle1 -PuzzleInput $puzzleInput
+$puzzle1Timer.Stop()
+
+Write-Host "Puzzle 1 Answer: $output"
+Write-Host "Time: $($puzzle1Timer.ElapsedMilliseconds) ms"
+
+$puzzle2Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle2 -PuzzleInput $puzzleInput
+$puzzle2Timer.Stop()
+
+Write-Host "Puzzle 2 Answer: $output"
+Write-Host "Time: $($puzzle2Timer.ElapsedMilliseconds) ms"
