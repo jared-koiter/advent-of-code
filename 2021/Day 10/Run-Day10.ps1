b134aebb3ac88232999311eb6da76caae4e82c78
Jared Koiter <jaredpkoiter@gmail.com>
2021-12-10 08:06:54 -0500

    2021-Day10 - Add puzzle 2 solution

--- 2021/Day 10/Run-Day10.ps1
+++ 2021/Day 10/Run-Day10.ps1
@@ -6,23 +6,23 @@ $bracketEndingLookup = @{
 }
 $startingBrackets = $bracketEndingLookup.Keys
 
-$bracketScoreLookup = @{
-    ')' = 3
-    ']' = 57
-    '}' = 1197
-    '>' = 25137
-}
-
 function Run-Puzzle1 {
     [CmdletBinding()]
     param (
         $PuzzleInput
     )
 
+    $bracketScoreLookup = @{
+        ')' = 3
+        ']' = 57
+        '}' = 1197
+        '>' = 25137
+    }
+
     $score = 0
     foreach ($line in $PuzzleInput) {
         [string[]]$chars = $line.ToCharArray()
-        [System.Collections.Generic.List[Char]] $chunkStack = @()
+        [System.Collections.Generic.List[string]] $chunkStack = @()
         foreach ($char in $chars) {
             if ($startingBrackets -contains $char) {
                 $chunkStack.Add($bracketEndingLookup.$char)
@@ -30,7 +30,7 @@ function Run-Puzzle1 {
             else {
                 $expectedClosingBracket = $chunkStack[$chunkStack.Count - 1]
                 if ($char -ne $expectedClosingBracket) {
-                    # corrupted chunk
+                    # corrupted chunk, add to total score
                     $score += $bracketScoreLookup.$char
                     break
                 }
@@ -48,7 +48,48 @@ function Run-Puzzle2 {
         $PuzzleInput
     )
 
-    #TODO
+    $bracketScoreLookup = @{
+        ')' = 1
+        ']' = 2
+        '}' = 3
+        '>' = 4
+    }
+
+    $lineScores = @()
+    foreach ($line in $PuzzleInput) {
+        [string[]]$chars = $line.ToCharArray()
+        [System.Collections.Generic.List[string]] $chunkStack = @()
+        $corrupted = $false
+        foreach ($char in $chars) {
+            if ($startingBrackets -contains $char) {
+                $chunkStack.Add($bracketEndingLookup.$char)
+            }
+            else {
+                $expectedClosingBracket = $chunkStack[$chunkStack.Count - 1]
+                if ($char -ne $expectedClosingBracket) {
+                    # corrupted chunk, skip this line
+                    $corrupted = $true
+                    break
+                }
+                $chunkStack.RemoveAt($chunkStack.Count - 1)
+            }
+        }
+
+        if ($corrupted) {
+            continue
+        }
+
+        # remaining brackets in the stack are the endings we need to add
+        $lineScore = 0
+        while ($chunkStack.Count -gt 0) {
+            $nextClosingBracket = $chunkStack[$chunkStack.Count - 1]
+            $lineScore = ($lineScore * 5) + $bracketScoreLookup.$nextClosingBracket
+            $chunkStack.RemoveAt($chunkStack.Count - 1)
+        }
+        $lineScores += $lineScore
+    }
+
+    return ($lineScores | Sort-Object)[[Math]::Floor($lineScores.Count / 2)]
 }
 
 [string[]]$puzzleInput = Get-Content .\input.txt
