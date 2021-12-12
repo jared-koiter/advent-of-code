0dade6583275e5b770ada6f565b50baf064a1c61
Jared Koiter <jaredpkoiter@gmail.com>
2021-12-12 14:13:34 -0500

    2021-Day12 - Submit puzzle 1 solution

--- 2021/Day 12/Run-Day12.ps1
+++ 2021/Day 12/Run-Day12.ps1
@@ -1,10 +1,87 @@
+function Get-CaveConnections {
+    [CmdletBinding()]
+    param (
+        $Connections
+    )
+    
+    $caves = @{}
+
+    foreach ($connection in $Connections) {
+        $start, $end = $connection -split '-'
+
+        if ($caves.$start) {
+            $caves.$start.Connections += $end
+        }
+        else {
+            $caves.$start = @{
+                IsLarge = ($start -cmatch '^[A-Z]*$')
+                Connections = @(
+                    $end
+                )
+            }
+        }
+
+        if ($caves.$end) {
+            $caves.$end.Connections += $start
+        }
+        else {
+            $caves.$end = @{
+                IsLarge = ($end -cmatch '^[A-Z]*$')
+                Connections = @(
+                    $start
+                )
+            }
+        }
+    }
+
+    return $caves
+}
+
+function Get-PathsFromNode {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        $Caves,
+        [Parameter(Mandatory = $true)]
+        $Node,
+        [Parameter(Mandatory = $false)]
+        [array]$Visited = @()
+    )
+
+    if ($Node -eq 'end') {
+        return @('end')
+    }
+
+    $Visited += $Node
+
+    $paths = @()
+    foreach ($connection in $Caves.$Node.Connections) {
+        # don't check visited small caves
+        if ($Visited -ccontains $connection -and (-not $Caves.$connection.IsLarge)) {
+            continue
+        }
+
+        [array]$possiblePaths = @(Get-PathsFromNode -Caves $Caves -Node $connection -Visited ($Visited | ForEach-Object { $_ }))
+
+        # append the current node to child paths to return all possible combinations
+        foreach ($possiblePath in $possiblePaths) {
+            $paths += ,@(@($Node) + $possiblePath)
+        }
+    }
+
+    return $paths
+}
+
 function Run-Puzzle1 {
     [CmdletBinding()]
     param (
         $PuzzleInput
     )
 
-    #TODO
+    $caves = Get-CaveConnections -Connections $PuzzleInput
+    $paths = Get-PathsFromNode -Caves $caves -Node 'start'
+
+    return $paths.Count
 }
 
 function Run-Puzzle2 {
