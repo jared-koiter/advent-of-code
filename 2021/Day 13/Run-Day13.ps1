8e1ae8204b786d838eec533fedbbe67aa60e3b7e
Jared Koiter <jaredpkoiter@gmail.com>
2021-12-13 08:25:31 -0500

    2021-Day13 - Submit puzzle 1 solution

--- 2021/Day 13/Run-Day13.ps1
+++ 2021/Day 13/Run-Day13.ps1
@@ -4,7 +4,52 @@ function Run-Puzzle1 {
         $PuzzleInput
     )
 
-    #TODO
+    $dots = @()
+    $instructions = @()
+    foreach ($line in $PuzzleInput) {
+        if ($line) {
+            if ($line -like 'fold along*') {
+                $null, $null, $instruction = $line -split ' '
+                $instructions += $instruction
+            }
+            else {
+                $x, $y = $line -split ','
+                $dots += @{
+                    x = [int]$x
+                    y = [int]$y
+                }
+            }
+        }
+    }
+
+    foreach ($instruction in $instructions) {
+        $foldDir, $coord = $instruction -split '='
+        $newDots = @()
+
+        if ($foldDir -eq 'x') {
+            $sameDir = 'y'
+        }
+        else {
+            $sameDir = 'x'
+        }
+
+        foreach ($dot in $dots) {
+            if ($dot.$foldDir -gt $coord) {
+                $newDots += @{
+                    $foldDir = ($coord - [Math]::Abs($dot.$foldDir - $coord))
+                    $sameDir = $dot.$sameDir
+                }
+            }
+            else {
+                $newDots += $dot
+            }
+        }
+
+        $dots = $newDots
+        break # part one only uses the first instruction
+    }
+
+    return ($dots | ForEach-Object { "$($_.x),$($_.y)" } | Select-Object -Unique).Count
 }
 
 function Run-Puzzle2 {
