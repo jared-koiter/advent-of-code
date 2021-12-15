d35f14f25a15a808d9041cdf3839b5387643ab16
Jared Koiter <jaredpkoiter@gmail.com>
2021-12-14 21:57:44 -0500

    2021-Day14 - Switch to faster method, add solution 2

--- 2021/Day 14/Run-Day14.ps1
+++ 2021/Day 14/Run-Day14.ps1
@@ -6,23 +6,50 @@ function Run-PolymerSteps {
         $RepetitionCount
     )
 
-    for ($i = 0; $i -lt $RepetitionCount; $i++) {
-        [System.Collections.ArrayList]$newPolymer = $Template | ForEach-Object { $_ }
+    # populate initial buckets
+    $pairBuckets = @{}
+    $letterBuckets = @{}
+    for ($i = 0; $i -lt ($Template.Count - 1); $i++) {
+        $letterBuckets.($Template[$i]) += 1
 
-        for ($j = ($template.Count - 2); $j -ge 0; $j--) {
-            $pair = "$($template[$j])$($template[$j+1])"
-            
-            if ($Rules.$pair) {
-                $newPolymer.Insert($j+1, $Rules.$pair)
-            }
+        $pair = "$($Template[$i])$($Template[$i+1])"
+        if ($pairBuckets.$pair) {
+            ($pairBuckets.$pair)++
         }
+        else {
+            $pairBuckets.$pair = 1
+        }
+    }
+    $letterBuckets.($Template[$i]) += 1
 
-        $Template = $newPolymer
+    for ($i = 0; $i -lt $RepetitionCount; $i++) {
+        $newPairBuckets = $pairBuckets.Clone()
+        foreach ($pair in $pairBuckets.Keys) {
+            $pairCount = $pairBuckets.$pair
+            [char]$newEntry = $Rules.$pair
+            $letterBuckets.$newEntry += $pairCount
+
+            $splitPair = $pair.ToCharArray()
+            $leftPair = "$($splitPair[0])$newEntry"
+            $rightPair = "$newEntry$($splitPair[1])"
+
+            if ($newPairBuckets.$pair -eq $pairCount) {
+                $newPairBuckets.Remove($pair)
+            }
+            else {
+                $newPairBuckets.$pair -= $pairCount
+            }
+
+            $newPairBuckets.$leftPair += $pairCount
+            $newPairBuckets.$rightPair += $pairCount
+        }
+        $pairBuckets = $newPairBuckets
     }
 
-    $counts = $Template | Group-Object -NoElement | ForEach-Object { $_.Count } | Measure-Object -Maximum -Minimum
+    $counts = $letterBuckets.Values | Measure-Object -Maximum -Minimum
     return ($counts.Maximum - $counts.Minimum)
 }
+
 
 function Run-Puzzle1 {
     [CmdletBinding()]
@@ -31,7 +58,7 @@ function Run-Puzzle1 {
     )
 
     $template = $PuzzleInput[0].ToCharArray()
-    $rules = @{}    
+    $rules = @{}
     $PuzzleInput[2..($PuzzleInput.Count - 1)] | ForEach-Object {
         $pair, $insert = $_ -split ' -> '
         $rules.$pair = $insert
@@ -46,7 +73,14 @@ function Run-Puzzle2 {
         $PuzzleInput
     )
 
-    #TODO
+    $template = $PuzzleInput[0].ToCharArray()
+    $rules = @{}
+    $PuzzleInput[2..($PuzzleInput.Count - 1)] | ForEach-Object {
+        $pair, $insert = $_ -split ' -> '
+        $rules.$pair = $insert
+    }
+
+    return Run-PolymerSteps -Template $template -Rules $rules -RepetitionCount 40
 }
 
 [string[]]$puzzleInput = Get-Content .\input.txt
