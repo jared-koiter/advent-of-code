afd3de8f3ec177d8590aa0990775362f39caf4e2
Jared Koiter <jaredpkoiter@gmail.com>
2022-12-02 07:34:41 -0500

    2022-Day1 - Add solution

---
+++ 2022/Day 1/Run-Day1.ps1
@@ -0,0 +1,59 @@
+function Run-Puzzle1 {
+    [CmdletBinding()]    
+    param (
+        $PuzzleInput
+    )
+
+    $index = 0
+    $calorieTotals = @(0)
+    foreach ($calorieCount in $PuzzleInput) {
+        if ($calorieCount -ne "") {
+            $calorieTotals[$index] += [int]$calorieCount
+        }
+        else {
+            $index++
+            $calorieTotals += 0
+        }
+    }
+
+    $calorieTotals = $calorieTotals | Sort-Object -Descending
+    return $calorieTotals[0]
+}
+
+function Run-Puzzle2 {
+    [CmdletBinding()]    
+    param (
+        $PuzzleInput
+    )
+
+    $index = 0
+    $calorieTotals = @(0)
+    foreach ($calorieCount in $PuzzleInput) {
+        if ($calorieCount -ne "") {
+            $calorieTotals[$index] += [int]$calorieCount
+        }
+        else {
+            $index++
+            $calorieTotals += 0
+        }
+    }
+
+    $calorieTotals = $calorieTotals | Sort-Object -Descending
+    return ($calorieTotals[0] + $calorieTotals[1] + $calorieTotals[2])
+}
+
+[string[]]$puzzleInput = Get-Content .\input.txt
+
+$puzzle1Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle1 -PuzzleInput $puzzleInput
+$puzzle1Timer.Stop()
+
+Write-Host "Puzzle 1 Answer: $output"
+Write-Host "Time: $($puzzle1Timer.ElapsedMilliseconds) ms"
+
+$puzzle2Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle2 -PuzzleInput $puzzleInput
+$puzzle2Timer.Stop()
+
+Write-Host "Puzzle 2 Answer: $output"
+Write-Host "Time: $($puzzle2Timer.ElapsedMilliseconds) ms"
