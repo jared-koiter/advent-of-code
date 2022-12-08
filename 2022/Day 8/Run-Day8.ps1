2b3c175edb05e8f9fc209b5006ad2d5d43b9e991
Jared Koiter <jaredpkoiter@gmail.com>
2022-12-08 08:04:51 -0500

    2022-Day8 - WIP Get map of input

---
+++ 2022/Day 8/Run-Day8.ps1
@@ -0,0 +1,49 @@
+function Get-HeightMap {
+    [CmdletBinding()]
+    param (
+        $MapInput
+    )
+
+    $map = @()
+    foreach ($line in $MapInput) {
+        $map += ,[int[]]($line -split '\B')
+    }
+
+    return $map
+}
+
+function Run-Puzzle1 {
+    [CmdletBinding()]
+    param (
+        $PuzzleInput
+    )
+
+    $map = Get-HeightMap -Input $PuzzleInput
+
+
+}
+
+function Run-Puzzle2 {
+    [CmdletBinding()]
+    param (
+        $PuzzleInput
+    )
+
+    #TODO
+}
+
+[int[]]$puzzleInput = Get-Content .\input.txt
+
+$puzzle1Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle1 -PuzzleInput $puzzleInput
+$puzzle1Timer.Stop()
+
+Write-Host "Puzzle 1 Answer: $output"
+Write-Host "Time: $($puzzle1Timer.ElapsedMilliseconds) ms"
+
+$puzzle2Timer = [System.Diagnostics.Stopwatch]::StartNew()
+$output = Run-Puzzle2 -PuzzleInput $puzzleInput
+$puzzle2Timer.Stop()
+
+Write-Host "Puzzle 2 Answer: $output"
+Write-Host "Time: $($puzzle2Timer.ElapsedMilliseconds) ms"
